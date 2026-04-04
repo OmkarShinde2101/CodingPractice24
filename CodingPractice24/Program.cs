2659810def73ececbd24a9d1679c9731d69877e9
Omkar Shinde <omkarshindee24@gmail.com>
2026-04-04 19:10:33 +0530

    This is todays code

--- CodingPractice24/Program.cs
+++ CodingPractice24/Program.cs
@@ -3,8 +3,37 @@ using System;
 
 class Program
 {
+    //static void Main()
+    //{
+    //    Console.WriteLine("Hello Coding Practice!");
+    //}
+
     static void Main()
     {
-        Console.WriteLine("Hello Coding Practice!");
+        //int[] arr1 = [1, 2, 3];
+        //int[] arr2 = [4, 5, 6];
+
+        //int[] merged = arr1.Concat(arr2).ToArray();
+
+        //foreach(int num in merged)
+        //{
+        //    Console.WriteLine(num);
+        //}
+
+        int[] arr1 = [1, 2, 3];
+        int[] arr2 = [4, 5, 6];
+
+        List<int> list = new List<int>();
+
+        list.AddRange(arr1);
+        list.AddRange(arr2);
+
+        int[] merged = list.ToArray();
+
+        foreach(int num in merged)
+        {
+            Console.WriteLine(num);
+        }
+
     }
 }
